f4775197b2a317402960af9fde1abafeef390c80
Jesus Palet <104393790+JeaLPaHu@users.noreply.github.com>
2025-08-13 21:13:19 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-</head>
-<body>
-    <header>
-        <h1></h1>
-    </header>
-    <main class="container">
-        <button onclick="mostarMensajeEnLaConsola()" class="button">Console</button>
-        <button onclick="mostarAlerta()" class="button">Alert</button>
-        <button onclick="mostrarPrompt()" class="button">Prompt</button>
-        <button onclick="sumaDosNumeros()" class="button">Suma</button>
-    </main>
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora del Desafío';
 
-      <script src="app.js"></script>
-</body>
-</html>
+function mostarMensajeEnLaConsola() {
+    console.log('El botón fue clicado!')
+}
+function mostrarPrompt() {
+    let ciudad = prompt("Por favor, ingresa el nombre de una ciudad de México:");
+    alert("Estuve en " + ciudad + " y me acordé de ti.");
+}
+
+function mostarAlerta(){
+    alert("AMO JS");
+} 
+
+function sumaDosNumeros(){
+    let primerNumero = parseInt(prompt('Digite el primer numero'));
+    let segundoNumero = parseInt(prompt('Digite el segundo numero'));
+    let resultado = primerNumero + segundoNumero;
+    alert(`${primerNumero} + ${segundoNumero} = ${resultado}`)
+}
